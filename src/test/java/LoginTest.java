9ab12f40bc78f94202eb068c6bc0d530f0bdc2b1
xkhottabx <50145870+xkhottabx@users.noreply.github.com>
2019-08-06 20:13:12 -0400

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -9,5 +9,7 @@ public class LoginTest {
         System.out.println("Line 5");
         System.out.println("Line 6");
         System.out.println("Line 7");
+        System.out.println("Line 8");
+        System.out.println("Line 9");
     }
 }
