cb15e7b7f0a502df2d73c1325800e08d45a39e64
xkhottabx <xkhottabx@gmail.com>
2019-08-13 19:42:01 -0400

    'c'

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,13 +3,11 @@ public class LoginTest {
 
     public static void main(String[] args) {
         System.out.println("Line 1");
-<<<<<<< HEAD
+
         System.out.println("Line 2");
         System.out.println("Line 3");
-=======
-      
->>>>>>> bc77fbb2f87aefdd5529a9f0b2fb7793d51de2b2
-      
+        System.out.println("Line 4");
+
 
     }
 }
