48d9441ffa65cf3247d4283c81b0719d230a5d21
xkhottabx <xkhottabx@gmail.com>
2019-08-13 20:22:13 -0400

    dev

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -7,6 +7,7 @@ public class LoginTest {
         System.out.println("Line 2");
         System.out.println("Line 3");
         System.out.println("Line 4");
+        System.out.println("Line 5");
 
 
     }
